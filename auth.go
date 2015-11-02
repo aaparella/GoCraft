17874170f50a541a1d7a3e4d222c65acec4d7082
Alex Parella <aaparella@ucdavis.edu>
2015-11-01 18:37:31 -0800

    Actually perform authentication check
    Return simple "could not authenticate" error message to client if there
    is a problem. Once the API is set up for logging in, we can provide more
    detailed and helpful error messages, or an error code of some sort for
    the client to interpret

--- auth.go
+++ auth.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 type AuthHello struct {
 	User string `json:"user"`
@@ -12,6 +15,10 @@ type AuthResponse struct {
 	Success bool `json:"success"`
 }
 
+/// Perform the authentication check
+/// Hits API to check if the credentials are valid
+/// Assumes any response other than a 200 is an indication
+/// that the crdentials provided were invalid
 func (p *Player) auth_hello(data []byte) {
 	var message AuthHello
 	if err := DecodeJSON(data, &message); err != nil {
@@ -19,13 +26,25 @@ func (p *Player) auth_hello(data []byte) {
 		p.SendError(err.Error())
 		return
 	}
-	// TODO(rweichler) : Hit the API lololol
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", "http://localhost:8080/login", nil)
+	req.Header.Set("username", message.User)
+	req.Header.Set("password", message.Pass)
+	resp, err := client.Do(req)
+
+	if err != nil || resp.StatusCode != 200 {
+		fmt.Println(err)
+		p.SendError("Could not authenticate")
+		return
+	}
+
 	global_room.AddPlayer(p)
 	p.Room = &global_room
-
 	response := AuthResponse{
 		LobbyID: 1,
 		Success: true,
 	}
 	p.SendMessage("auth_response", response)
+	resp.Body.Close()
 }
